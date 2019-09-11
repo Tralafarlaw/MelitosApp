7881e2bf4852600a4edfdfef95a7d956b9e204c4
Tralafarlaw <tralafarlaw@gmail.com>
2019-09-11 15:18:29 -0400

    bottom ready

--- app/src/main/java/com/amuyu/melitos/MenuActivity.java
+++ app/src/main/java/com/amuyu/melitos/MenuActivity.java
@@ -10,30 +10,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 public class MenuActivity extends AppCompatActivity implements OnBubbleClickListener {
-    AppBarConfiguration appBarConfiguration;
+
     NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-       // BottomNavigationView navView = findViewById(R.id.nav_view);
         BubbleTabBar sd = findViewById(R.id.nav_view);
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-
-        appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard,R.id.navigation_sales, R.id.navigation_notifications, R.id.navigation_settings)
-                .build();
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);}
 
-        //NavigationUI.setupWithNavController(navView, navController);
         sd.addBubbleListener(this);
     }
 
@@ -44,7 +33,6 @@ public class MenuActivity extends AppCompatActivity implements OnBubbleClickList
 
     @Override
     public void onBubbleClick(int i) {
-        NavigationUI.navigateUp(navController, appBarConfiguration);
         navController.navigate(i);
     }
 }
---
+++ app/src/main/java/com/amuyu/melitos/ui/ProductoDialog/ProductDIalog.java
@@ -0,0 +1,38 @@
+package com.amuyu.melitos.ui.ProductoDialog;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.amuyu.melitos.R;
+
+public class ProductDIalog extends Fragment {
+
+    private ProductDialogViewModel mViewModel;
+
+    public static ProductDIalog newInstance() {
+        return new ProductDIalog();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.product_dialog_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ProductDialogViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
