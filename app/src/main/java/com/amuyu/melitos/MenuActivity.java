1dbd7ce2d4cfe639a110a34703553ad857ca0e11
Tralafarlaw <tralafarlaw@gmail.com>
2019-09-11 02:18:43 -0400

    bottom ready

--- app/src/main/java/com/amuyu/melitos/MenuActivity.java
+++ app/src/main/java/com/amuyu/melitos/MenuActivity.java
@@ -2,6 +2,9 @@ package com.amuyu.melitos;
 
 import android.os.Bundle;
 
+import com.fxn.BubbleTabBar;
+import com.fxn.OnBubbleClickListener;
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +13,38 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MenuActivity extends AppCompatActivity {
-
+public class MenuActivity extends AppCompatActivity implements OnBubbleClickListener {
+    AppBarConfiguration appBarConfiguration;
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+       // BottomNavigationView navView = findViewById(R.id.nav_view);
+        BubbleTabBar sd = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard,R.id.navigation_sales, R.id.navigation_notifications, R.id.navigation_settings)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);}
+
+        //NavigationUI.setupWithNavController(navView, navController);
+        sd.addBubbleListener(this);
     }
 
     @Override
     public void onBackPressed() {
 
+    }
+
+    @Override
+    public void onBubbleClick(int i) {
+        NavigationUI.navigateUp(navController, appBarConfiguration);
+        navController.navigate(i);
     }
 }
---
+++ app/src/main/java/com/amuyu/melitos/ui/Settings/SettingsViewModel.java
@@ -0,0 +1,7 @@
+package com.amuyu.melitos.ui.Settings;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
