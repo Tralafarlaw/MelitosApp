4b9c66c866aaadde858a1a43728a0fbb2dd0209c
TheProdigyK <luisale0410@gmail.com>
2019-09-12 23:17:55 -0400

    Fragment de ventas con idea principal lista. (falta chips)

--- app/src/main/java/com/amuyu/melitos/ui/vender/ventas.java
+++ app/src/main/java/com/amuyu/melitos/ui/vender/ventas.java
@@ -2,6 +2,10 @@ package com.amuyu.melitos.ui.vender;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,12 +15,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amuyu.melitos.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 public class ventas extends Fragment {
 
     private VentasViewModel mViewModel;
+    Button btnLookup;
+    List<Item> items;
+    ListView listView;
+    ItemsListAdapter myItemsListAdapter;
 
     public static ventas newInstance() {
         return new ventas();
@@ -25,6 +46,7 @@ public class ventas extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.ventas_fragment, container, false);
     }
 
@@ -33,6 +55,184 @@ public class ventas extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(VentasViewModel.class);
         // TODO: Use the ViewModel
+        listView = (ListView)getView().findViewById(R.id.listview);
+        btnLookup = (Button)getView().findViewById(R.id.lookup);
+
+        initItems();
+        myItemsListAdapter = new ItemsListAdapter(getActivity(), items);
+        listView.setAdapter(myItemsListAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getActivity(),
+                        ((Item)(parent.getItemAtPosition(position))).ItemString,
+                        Toast.LENGTH_LONG).show();
+            }});
+
+        btnLookup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = "Check items:\n";
+
+                for (int i=0; i<items.size(); i++){
+                    if (items.get(i).isChecked()){
+                        str += i + "\n";
+                    }
+                }
+
+                /*
+                int cnt = myItemsListAdapter.getCount();
+                for (int i=0; i<cnt; i++){
+                    if(myItemsListAdapter.isChecked(i)){
+                        str += i + "\n";
+                    }
+                }
+                */
+
+                Toast.makeText(getActivity(),
+                        str,
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    public class Item {
+        boolean checked;
+        Drawable ItemDrawable;
+        String ItemString;
+        String ItemPrice;
+        Item(Drawable drawable, String t, String p, boolean b){
+            ItemDrawable = drawable;
+            ItemString = t;
+            checked = b;
+            ItemPrice = p;
+        }
+
+        public boolean isChecked(){
+            return checked;
+        }
+    }
+
+    static class ViewHolder {
+        CheckBox checkBox;
+        ImageView icon;
+        TextView text, text2;
+    }
+
+    public class ItemsListAdapter extends BaseAdapter {
+
+        private Context context;
+        private List<Item> list;
+
+        ItemsListAdapter(Context c, List<Item> l) {
+            context = c;
+            list = l;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public boolean isChecked(int position) {
+            return list.get(position).checked;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View rowView = convertView;
+
+            // reuse views
+            ViewHolder viewHolder = new ViewHolder();
+            if (rowView == null) {
+                LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+                rowView = inflater.inflate(R.layout.vender_item, null);
+
+                viewHolder.checkBox = (CheckBox) rowView.findViewById(R.id.rowCheckBox);
+                viewHolder.icon = (ImageView) rowView.findViewById(R.id.rowImageView);
+                viewHolder.text = (TextView) rowView.findViewById(R.id.rowTextView);
+                viewHolder.text2 = (TextView) rowView.findViewById(R.id.rowTextView2);
+                rowView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) rowView.getTag();
+            }
+
+            viewHolder.icon.setImageDrawable(list.get(position).ItemDrawable);
+            viewHolder.checkBox.setChecked(list.get(position).checked);
+
+            final String itemStr = list.get(position).ItemString;
+            viewHolder.text.setText(itemStr);
+
+            final String itemPrc = list.get(position).ItemPrice;
+            viewHolder.text2.setText(itemPrc);
+
+            viewHolder.checkBox.setTag(position);
+
+            /*
+            viewHolder.checkBox.setOnCheckedChangeListener(
+                    new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    list.get(position).checked = b;
+
+                    Toast.makeText(getApplicationContext(),
+                            itemStr + "onCheckedChanged\nchecked: " + b,
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+            */
+
+            viewHolder.checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    boolean newState = !list.get(position).isChecked();
+                    list.get(position).checked = newState;
+//                    Toast.makeText(getActivity(),
+//                            itemStr + "setOnClickListener\nchecked: " + newState,
+//                            Toast.LENGTH_LONG).show();
+                }
+            });
+
+            viewHolder.checkBox.setChecked(isChecked(position));
+
+            return rowView;
+        }
+    }
+
+
+
+    private void initItems(){
+        items = new ArrayList<Item>();
+
+        TypedArray arrayDrawable = getResources().obtainTypedArray(R.array.resicon);
+        TypedArray arrayText = getResources().obtainTypedArray(R.array.restext);
+        TypedArray arrayPrice = getResources().obtainTypedArray(R.array.resPrice);
+
+        for(int i=0; i<arrayDrawable.length(); i++){
+            Drawable d = arrayDrawable.getDrawable(i);
+            String s = arrayText.getString(i);
+            String p = arrayPrice.getString(i);
+            boolean b = false;
+            Item item = new Item(d, s, p, b);
+            items.add(item);
+        }
+
+        arrayDrawable.recycle();
+        arrayText.recycle();
     }
 
 }
