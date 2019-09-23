c85b5119a04598ed589902d3256be81a6f6d2b31
TheProdigyK <luisale0410@gmail.com>
2019-09-23 19:33:25 -0400

    Agregue 3 xml nuevos, carrito_fragment (utilizar mismo item vender_item.xml), para mostrar mas detalles cada item tiene que tener su despegable, y su diseño esta en carrito_item2 (ver kyte). Descuento_fragment.xml creado
    HomeFragment.java lo utilizé para pruebas, ahí esta el codigo de la camara o seleccionar imagen de galeria (lo revisas :u no se donde está fallando), tambien los permisos necesarios

--- app/src/main/java/com/amuyu/melitos/ui/home/HomeFragment.java
+++ app/src/main/java/com/amuyu/melitos/ui/home/HomeFragment.java
@@ -1,28 +1,62 @@
 package com.amuyu.melitos.ui.home;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.amuyu.melitos.BuildConfig;
 import com.amuyu.melitos.R;
+import com.yalantis.ucrop.UCrop;
+
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static android.app.Activity.RESULT_OK;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private static final int CAMERA_ACTION_PICK_REQUEST_CODE = 610;
+    private static final int PICK_IMAGE_GALLERY_REQUEST_CODE = 609;
+    public static final int CAMERA_STORAGE_REQUEST_CODE = 611;
+    public static final int ONLY_CAMERA_REQUEST_CODE = 612;
+    public static final int ONLY_STORAGE_REQUEST_CODE = 613;
+    ImageView imageView;
+    ImageButton imageButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.new_p_fragment1, container, false);
         /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -30,6 +64,113 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });*/
+        imageView = getActivity().findViewById(R.id.imageView3);
+        imageButton = getActivity().findViewById(R.id.imageButton2);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                checkSelfPermissions(getActivity());
+                openCamera();
+            }
+        });
+
         return root;
     }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public boolean checkSelfPermissions(@NonNull Activity activity) {
+        if (activity.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, CAMERA_STORAGE_REQUEST_CODE);
+            return false;
+        } else if (activity.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, ONLY_CAMERA_REQUEST_CODE);
+            return false;
+        } else if (activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, ONLY_STORAGE_REQUEST_CODE);
+            return false;
+        }
+        return true;
+    }
+
+    private void openCamera() {
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = getImageFile();
+        Uri uri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            uri = FileProvider.getUriForFile(getContext(), BuildConfig.APPLICATION_ID.concat(".provider"), file);
+        else
+            uri = Uri.fromFile(file);
+        pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(pictureIntent, CAMERA_ACTION_PICK_REQUEST_CODE);
+    }
+
+    String currentPhotoPath = "";
+    private File getImageFile() {
+        String imageFileName = "JPEG_" + System.currentTimeMillis() + "_";
+        File storageDir = new File(
+                Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_DCIM
+                ), "Camera"
+        );
+        File file = null;
+        try {
+            file = File.createTempFile(
+                    imageFileName, ".jpg", storageDir
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        currentPhotoPath = "file:" + file.getAbsolutePath();
+        return file;
+    }
+
+    private void openCropActivity(Uri sourceUri, Uri destinationUri) {
+        UCrop.of(sourceUri, destinationUri)
+                .withMaxResultSize(500  , 500)
+                .withAspectRatio(5f, 5f)
+                .start(getActivity());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == CAMERA_ACTION_PICK_REQUEST_CODE && resultCode == RESULT_OK) {
+            Uri uri = Uri.parse(currentPhotoPath);
+            openCropActivity(uri, uri);
+        } else if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
+            Uri uri = UCrop.getOutput(data);
+            showImage(uri);
+        } else if (requestCode == PICK_IMAGE_GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri sourceUri = data.getData();
+            File file = getImageFile();
+            Uri destinationUri = Uri.fromFile(file);
+            openCropActivity(sourceUri, destinationUri);
+        }
+    }
+
+    private void showImage(Uri imageUri) {
+//        File file = FileUtils.getFile(getContext(), imageUri);
+//        InputStream inputStream = null;
+//        try {
+//            inputStream = new FileInputStream(file);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+//
+//        imageView.setImageBitmap(bitmap);
+        imageView.setImageURI(imageUri);
+    }
+
+    private void openImagesDocument() {
+        Intent pictureIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        pictureIntent.setType("image/*");  // 1
+        pictureIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            String[] mimeTypes = new String[]{"image/jpeg", "image/png"};
+            pictureIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        }
+        startActivityForResult(Intent.createChooser(pictureIntent,"Select Picture"), PICK_IMAGE_GALLERY_REQUEST_CODE);
+    }
+
 }
